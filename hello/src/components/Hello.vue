625a72b0e95d7cdca6e86fe39b4b863cf8bffc39
Vitor Manfré <vitormd97@gmail.com>
2017-05-04 21:33:04 -0300

    hello webpack added

---
+++ hello/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Bem vindo ao seu app com Vue.js'
+    }
+  }
+}
+</script>
